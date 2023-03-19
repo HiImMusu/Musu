70ba4bf26d1cfd7b5c92a84d0107f76a459b9e66
Daniel <domonicdawkins24@gmail.com>
2023-03-19 07:37:36 -0500

    V15

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const express = require("express");
-const https = require("https");
-const fs = require("fs");
-const port = process.env.PORT || 3000;
-const APIKEY = process.env.APIKEY;
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send(JSON.stringify({
-        SUCCESS: true,
-        MESSAGE: "Welcome to Halla City!"
-    }));
-})
-
-app.get("/GetCatalogItems", (req, res) => {
-    const url = "https://catalog.roblox.com/v2/search/items/details?Category=3&Subcategory=56&salesTypeFilter=1&&Keyword=Shirt&Limit=10";
+app.get("/GetShirts", (req, res) => {
+    const url = "https://catalog.roblox.com/v2/search/items/details?Category=3&Subcategory=56&salesTypeFilter=1&&Keyword=Shirt&Limit=30";
 
     https.get(url, (response) => {
         let data = "";
@@ -30,9 +16,76 @@ app.get("/GetCatalogItems", (req, res) => {
     }).on("error", (error) => {
         res.status(500).send(JSON.stringify({
             SUCCESS: false,
-            MESSAGE: "Failed to fetch catalog items"
+            MESSAGE: "Failed to fetch shirts"
         }));
     });
 });
 
-app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
+app.get("/GetHairs", (req, res) => {
+    const url = "https://catalog.roblox.com/v2/search/items/details?Category=2&Subcategory=2&salesTypeFilter=1&&Keyword=Hair&Limit=30";
+
+    https.get(url, (response) => {
+        let data = "";
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+        response.on("end", () => {
+            const results = JSON.parse(data);
+            res.status(200).send(JSON.stringify({
+                SUCCESS: true,
+                MESSAGE: results
+            }));
+        });
+    }).on("error", (error) => {
+        res.status(500).send(JSON.stringify({
+            SUCCESS: false,
+            MESSAGE: "Failed to fetch hairs"
+        }));
+    });
+});
+
+app.get("/GetPants", (req, res) => {
+    const url = "https://catalog.roblox.com/v2/search/items/details?Category=3&Subcategory=57&salesTypeFilter=1&&Keyword=Pants&Limit=30";
+
+    https.get(url, (response) => {
+        let data = "";
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+        response.on("end", () => {
+            const results = JSON.parse(data);
+            res.status(200).send(JSON.stringify({
+                SUCCESS: true,
+                MESSAGE: results
+            }));
+        });
+    }).on("error", (error) => {
+        res.status(500).send(JSON.stringify({
+            SUCCESS: false,
+            MESSAGE: "Failed to fetch pants"
+        }));
+    });
+});
+
+app.get("/GetFaces", (req, res) => {
+    const url = "https://catalog.roblox.com/v2/search/items/details?Category=2&Subcategory=3&salesTypeFilter=1&&Keyword=Face&Limit=30";
+
+    https.get(url, (response) => {
+        let data = "";
+        response.on("data", (chunk) => {
+            data += chunk;
+        });
+        response.on("end", () => {
+            const results = JSON.parse(data);
+            res.status(200).send(JSON.stringify({
+                SUCCESS: true,
+                MESSAGE: results
+            }));
+        });
+    }).on("error", (error) => {
+        res.status(500).send(JSON.stringify({
+            SUCCESS: false,
+            MESSAGE: "Failed to fetch faces"
+        }));
+    });
+});
