88ab12cf437dd805f4093ae00836f480dbd82ad8
Daniel <domonicdawkins24@gmail.com>
2023-03-19 07:47:47 -0500

    V17

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const port = process.env.PORT || 3000;
+const APIKEY = process.env.APIKEY;
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(JSON.stringify({
+        SUCCESS: true,
+        MESSAGE: "Welcome to Halla City!"
+    }));
+})
+
 app.get("/GetShirts", (req, res) => {
     const url = "https://catalog.roblox.com/v2/search/items/details?Category=3&Subcategory=56&salesTypeFilter=1&&Keyword=Shirt&Limit=30";
 
@@ -89,3 +103,5 @@ app.get("/GetFaces", (req, res) => {
         }));
     });
 });
+
+app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
